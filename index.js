b24cf9bcd9d78d168c25ace0c7ef538f53e3e813
danvarga1985 <danvarga1985@gmail.com>
2020-05-28 08:30:50 +0200

    set end-points, response, basic implementation of routing

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
 const laptopData = JSON.parse(json);
 
 //Create server
 const server = http.createServer((req, res) => {
-    console.log('Someone did access the server');
+    
+    const pathName = url.parse(req.url, true).pathname;
+    const id = url.parse(req.url, true).query.id;
+
+    if (pathName === '/products' || pathName == '/') {
+        //Create response for interactions -> header with HTTP status code
+        res.writeHead(200, { 'Content-type': 'text/html'} );
+        res.end('This is the Product page!');
+    } 
+    
+    else if (pathName === '/laptop' && id < laptopData.length) {
+        res.writeHead(200, { 'Content-type': 'text/html'} );
+        res.end(`This is the Laptop page for laptop ${id}!`);
+    } 
+    
+    else {
+        res.writeHead(404, { 'Content-type': 'text/html'} );
+        res.end('URL was not found on the server');
+    }
+
 });
 
 server.listen(1337, '127.0.0.1', () => {
